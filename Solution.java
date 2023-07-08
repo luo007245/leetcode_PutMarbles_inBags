4a26e52de8f4c337195d98821e1e430a04bb83b1
Ankush Gaur <MY_NAME@example.com>
2023-07-08 13:32:36 +0530

    Put Marbles in Bags Day#-8

---
+++ Solution.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    public long putMarbles(int[] weights, int k) {
+        List<Integer> sums = new ArrayList<>();
+    
+
+        for(int i =1; i < weights.length; i++){
+            sums.add(weights[i] + weights[ i -1]);
+        }
+
+        Collections.sort(sums);
+        
+        long smaller = 0;
+        
+        for (int i = 0; i < k - 1; ++i) {
+            smaller += sums.get(i);
+        }
+        
+        long larger = 0;
+        
+        Collections.reverse(sums);
+
+
+        for (int i = 0; i < k - 1; ++i) {
+            larger += sums.get(i);
+        }
+        
+        
+        return larger - smaller;
+    }
+    
+}
